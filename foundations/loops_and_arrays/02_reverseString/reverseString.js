b3c735a1d498add3b29da00f62f4f23f95475a9d
yhm88 <261919706+yhm88@users.noreply.github.com>
2026-09-17 14:11:16 +0800

    Familier with the process of loop more
    Try loop and some array method to reverse string.

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    result = string[i] + result;
+  }
+  return result;
 };
 
 // Do not edit below this line
