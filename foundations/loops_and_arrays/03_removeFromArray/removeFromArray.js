3634cda21f76ddc25ae53451d26af1eb5fdcaa64
yhm88 <261919706+yhm88@users.noreply.github.com>
2026-09-17 20:45:03 +0800

    Use method and arguments to remove from array

--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr) {
+    blacklists = Array.from(arguments).slice(1);
+    return arr.filter(item => !blacklists.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
