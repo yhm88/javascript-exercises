74f2e557395ab66d20803e4796148080dd951a04
yhm88 <261919706+yhm88@users.noreply.github.com>
2026-09-18 21:36:21 +0800

    Learn the destructuring to sum the num

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
 
+  if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return 'ERROR'
+  } else if (num1 < num2) {
+    for (i = num1; i <= num2; i++) {
+      sum += i;
+    };
+  } else if (num1 > num2) {
+    for (i = num1; i >= num2; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
