92b5255e6780e7fd71ddd7f28e19a1cd09c7ed8e
yhm88 <261919706+yhm88@users.noreply.github.com>
2026-09-18 22:37:55 +0800

    Clear the whitespace

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -2,7 +2,6 @@ const leapYears = function(year) {
   if ( year % 4 === 0 && year % 100 !== 0 ) return true;
   if (year % 400 === 0) return true;
   return false;
-  
 }
 // Do not edit below this line
 module.exports = leapYears;
